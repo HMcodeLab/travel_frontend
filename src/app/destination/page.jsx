b3596734f624eabf81c9d8133c33bd6f872dfc2e
info@hmcodelab.com <info@hmcodelab.com>
2024-05-22 17:35:13 +0530

    updated

--- src/app/destination/page.jsx
+++ src/app/destination/page.jsx
@@ -6,11 +6,11 @@ import Image from 'next/image'
 const Destination = () => {
   return (
     <>
-    <div className='h-[70vh] w-full flex justify-center items-center relative'>
+    <div className='h-[70vh] w-full  relative'>
         <div className='absolute top-0 w-full h-full'>
             <Image src={Bg} className='brightness-50'/>
         </div>
-        <div className='absolute top-[30%] text-white space-y-4'>
+        <div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white space-y-4'>
             <p className='text-white font-extrabold text-5xl text-center'>Ladakh</p>
             <p className='text-3xl border-b-[1px] border-[#B6B6B696]'>Thrills for Soul and Spirit!</p>
             <p className='flex justify-center text-center gap-1 p-1 font-semibold text-xl'>
@@ -18,7 +18,7 @@ const Destination = () => {
                 <span className='bg-[#CA1C26B2] px-1'>60% off</span>
             </p>
             <div className='flex justify-center w-full'><Link href='/' className='px-5 py-2 bg-red-700 text-white '>EXPLORE LADAKH</Link></div>
-            <div className='flex justify-center w-full '><Image className='translate-y-2 h-16' src={Down}/></div>
+            <Image className='absolute top-[100%] translate-y-7 left-[50%] h-16' src={Down}/>
         </div>
     </div>
     </>
