1c5c224fd0a993ced4cd0469de2f051b1634a6d2
AnshulKaryal <karyalanshul@gmail.com>
2024-05-28 18:22:00 +0530

    Contact Us responsive and Activity Page

--- src/components/navbar/Navmenu.js
+++ src/components/navbar/Navmenu.js
@@ -18,7 +18,7 @@ const Navmenu = () => {
             </div>
             <div className={styles.menu_btn_section}>
                 <span>
-                    <button>TOUR</button>
+                    <Link href={'/'}>TOUR</Link>
                     <Link href={'/activities'}>ACTIVITIES</Link>
                 </span>
             </div>
