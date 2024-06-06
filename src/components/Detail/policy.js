fdb8a437a9985594a9c073965389145ab6714c83
vivek kumar <vivekr4400@gmail.com>
2024-06-06 17:15:25 +0530

    updated

--- src/components/Detail/policy.js
+++ src/components/Detail/policy.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Policy = ({ data }) => {
 
-
+    if (data === undefined) {
+        return (
+            <>Loading...</>
+        )
+    }
 
     return (
         <div>
