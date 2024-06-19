969d8c0ba163ef962380d15701a0cfedea21b646
vivek kumar <vivekr4400@gmail.com>
2024-06-19 14:33:57 +0530

    updated search

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,59 +1,63 @@
-"use client"
+"use client";
 
-import React, { Suspense, useEffect, useState } from 'react'
-import { useSearchParams } from 'next/navigation'
+import React, { useEffect, useState, Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
 import Cards from '@/components/card/page';
 import axios from 'axios';
-import styles from '../../components/Home/section6/section6.module.css'
+import styles from '../../components/Home/section6/section6.module.css';
 
 const Search = () => {
-    const [cardsData, setCardsData] = useState();
+    const [cardsData, setCardsData] = useState(null);
+    const [loading, setLoading] = useState(true);
     const searchParams = useSearchParams();
     const search = JSON.parse(searchParams.get('data'));
 
     useEffect(() => {
-        handleSearch()
-    }, [])
+        const handleSearch = async () => {
+            try {
+                const apiUrl =
+                    "https://staging.trackitinerary.com/apis/packages/search_filter_packages";
+                const res = await axios.post(apiUrl, search, {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
 
-    const handleSearch = async () => {
-        try {
-            const apiUrl =
-                "https://staging.trackitinerary.com/apis/packages/search_filter_packages";
-            const res = await axios.post(apiUrl, search, {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
+                console.log(res);
 
-            console.log(res);
-
-            if (res.data.status === 200) {
-                setCardsData(res.data.data);
+                if (res.data.status === 200) {
+                    setCardsData(res.data.data);
+                }
+            } catch (error) {
+                console.log(error);
+                // Handle error gracefully
+            } finally {
+                setLoading(false);
             }
-        } catch (error) {
-            console.log(error);
-            toast.error("Failed To send Query");
-        }
+        };
+
+        handleSearch();
+    }, [search]);
+
+    if (loading) {
+        return <div>Loading...</div>;
     }
 
-    // console.log(cardsData)
     return (
-        <Suspense fallback={<div>Loading...</div>} >
-            <div className='px-[10vw]'>
-                <div className={styles.all_cards_main}>
-                    {
-                        cardsData?.map((cardData, ind) => {
-                            return (
-                                <>
-                                    <Cards key={ind} val={cardData} />
-                                </>
-                            )
-                        })
-                    }
-                </div>
+        <div className='px-[10vw]'>
+            <div className={styles.all_cards_main}>
+                {cardsData?.map((cardData, ind) => (
+                    <Cards key={ind} val={cardData} />
+                ))}
             </div>
-        </Suspense >
-    )
-}
+        </div>
+    );
+};
 
-export default Search
+const SearchPage = () => (
+    <Suspense fallback={<div>Loading...</div>}>
+        <Search />
+    </Suspense>
+);
+
+export default SearchPage;
