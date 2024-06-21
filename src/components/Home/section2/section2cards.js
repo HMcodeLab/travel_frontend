fee3bbfd705e4e6326caad6385d4a64d3542b8ea
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-06-21 17:13:06 +0530

    changes

--- src/components/Home/section2/section2cards.js
+++ src/components/Home/section2/section2cards.js
@@ -27,71 +27,71 @@ const Section2Cards = () => {
 
     return (
         <>
-         
-        <div className={styles.card_section_main}>
-            <div className={styles.vertical_cards}>
-                {
-                    movingCard?.slice(0, 3).map((val, ind) => {
-                        return (
-                            <div>
-                                <Link href={`/destination/?city_name=${val.name}&cityid=${val.city_id}`}>
-                                <Image src={val.image} height={1000} width={1000} />
-                                <p>{val?.name}</p>
-                                </Link>
-                          
-                            </div>
-                        )
-                    })
-                }
+
+            <div className={styles.card_section_main}>
+                <div className={styles.vertical_cards}>
+                    {
+                        movingCard?.slice(0, 3).map((val, ind) => {
+                            return (
+                                <div>
+                                    <Link href={`/destination/?city_name=${val.name}&cityid=${val.city_id}`}>
+                                        <Image src={val.image} height={1000} width={1000} />
+                                        <p>{val?.name}</p>
+                                    </Link>
+
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className={styles.anim_cards}>
+                    <span>
+                        <div className={styles.places_card}>
+                            {
+                                movingCard?.slice(3, 9)?.map((data, ind) => {
+                                    return (
+                                        <div>
+                                            <Link href={`/destination/?city_name=${data?.name}&cityid=${data?.city_id}`} key={ind}>
+                                                <Image src={data.image} height={1000} width={1000} />
+                                                <p>{data?.name}</p>
+                                            </Link>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div className={`${styles.places_card} ${styles.places2}`}>
+                            {
+                                movingCard?.slice(3, 9)?.map((data, ind) => {
+                                    return (
+                                        <div>
+                                            <Link href={`/destination/?city_name=${data?.name}&cityid=${data?.city_id}`} key={ind}>
+                                                <Image src={data.image} height={1000} width={1000} />
+                                                <p>{data?.name}</p>
+                                            </Link>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div className={styles.places_card}>
+                            {
+                                movingCard?.slice(3, 9)?.map((data, ind) => {
+                                    return (
+                                        <div>
+                                            <Link href={`/destination/?city_name=${data?.name}&cityid=${data?.city_id}`} key={ind}>
+                                                <Image src={data.image} height={1000} width={1000} />
+                                                <p>{data?.name}</p>
+                                            </Link>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </span>
+                </div>
             </div>
-            <div className={styles.anim_cards}>
-                <span>
-                    <div className={styles.places_card}>
-                        {
-                            movingCard?.slice(3, 9)?.map((data, ind) => {
-                                return (
-                                    <div>
-                                        <Link href={`/destination/?city_name=${data?.name}&cityid=${data?.city_id}`}>
-                                        <Image src={data.image} height={1000} width={1000} />
-                                        <p>{data?.name}</p>
-                                        </Link>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                    <div className={`${styles.places_card} ${styles.places2}`}>
-                        {
-                            movingCard?.slice(3, 9)?.map((data, ind) => {
-                                return (
-                                    <div>
-                                           <Link href={`/destination/?city_name=${data?.name}&cityid=${data?.city_id}`}>
-                                        <Image src={data.image} height={1000} width={1000} />
-                                        <p>{data?.name}</p>
-                                        </Link>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                    <div className={styles.places_card}>
-                        {
-                            movingCard?.slice(3, 9)?.map((data, ind) => {
-                                return (
-                                    <div>
-                                           <Link href={`/destination/?city_name=${data?.name}&cityid=${data?.city_id}`}>
-                                        <Image src={data.image} height={1000} width={1000} />
-                                        <p>{data?.name}</p>
-                                        </Link>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                </span>
-            </div>
-        </div>
-        
+
         </>
     )
 }
