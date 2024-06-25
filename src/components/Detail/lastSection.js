60d6e8c3981b7ec954268a752cb57945498efd10
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-06-25 17:30:53 +0530

    css chnages

--- src/components/Detail/lastSection.js
+++ src/components/Detail/lastSection.js
@@ -10,8 +10,8 @@ const LastSection = ({ data }) => {
     return (
         <div className='w-full flex justify-between xsm:flex-col xsm:overflow-hidden' >
             <div className='flex flex-col w-[30%]  flex-wrap gap-3 xsm:w-[80%]'>
-                <div className='uppercase border-l-4 border-[var(--primary)]  font-bold text-2xl  flex flex-wrap pl-1 w-[70%] xsm:w-[90%] text-[1.0rem]'>
-                    <p>Trending, Best Selling <span className='text-[var(--primary)]'>Tours</span> And Fun <span className='text-[var(--primary)]'>Destinations</span></p>
+                <div className='uppercase border-l-4 border-[var(--primary)]  font-bold text-2xl  flex flex-wrap pl-1 w-[80%] xsm:w-[90%] '>
+                    <p className='lastsection_left_heading'>Trending, Best Selling <span className='text-[var(--primary)]'>Tours</span> And Fun <span className='text-[var(--primary)]'>Destinations</span></p>
                 </div>
                 <p className='text-[#494545] xsm:hidden'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, sunt?</p>
             </div>
