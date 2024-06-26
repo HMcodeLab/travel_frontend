1ef1265f2d3779c13ed3704f3374aa809c0511d7
vivek kumar <vivekr4400@gmail.com>
2024-06-26 17:17:35 +0530

    added pagination

--- src/app/all_destination/page.js
+++ src/app/all_destination/page.js
@@ -1,12 +1,42 @@
 "use client"
 
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { GlobalProvider } from '../layout'
 import Cards from '@/components/card/page'
+import Pagination from '@/components/Pagination/pagination'
+import { BASE_URL } from '@/helpers/baseurl'
 
 const All_destination = () => {
-    const { Fetchcards, CardsData } = useContext(GlobalProvider)
-  return (
+    let PageSize = 10;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [CardsData, setCardsData] = useState([])
+
+
+    const Fetchcards = async () => {
+        
+        try {
+            const data = await fetch(BASE_URL + `/index?page=${currentPage}&limit=9`)
+            const response = await data.json();
+            setCardsData(response?.data)
+            window.scrollTo(0,0)
+
+        } catch (error) {
+            console.log(error);
+            return <h3>An Error Occured . Try Refreshing the Page...</h3>
+        }
+    }
+    useEffect(() => {
+        Fetchcards()
+    }, [currentPage])
+
+
+    if (!CardsData) {
+       return <h3>Loading...</h3>
+   }
+  
+
+    return (
+      <>
       <div className='grid grid-cols-3 gap-[2.5rem] mt-[3vh] px-[10vw]'>
           {
               CardsData?.data?.map((val, ind) => {
@@ -15,7 +45,15 @@ const All_destination = () => {
                   )
               })
           }
-      </div>
+            </div>
+            <Pagination
+                className="pagination-bar"
+                currentPage={currentPage}
+                totalCount={CardsData?.totalCount}
+                pageSize={PageSize}
+                onPageChange={page => setCurrentPage(page)}
+            />
+        </>
   )
 }
 
