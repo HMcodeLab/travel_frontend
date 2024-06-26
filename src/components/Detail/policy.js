7d5898a17c7534d0e5e1f6d135587f01ec8157e8
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-06-26 14:26:17 +0530

    css chnages

--- src/components/Detail/policy.js
+++ src/components/Detail/policy.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import styles from './page.module.css'
 const Policy = ({ data }) => {
 
     if (data === undefined) {
@@ -9,7 +9,7 @@ const Policy = ({ data }) => {
     }
 
     return (
-        <div>
+        <div className={styles.policy_tab_content_wrapper} >
             <h2 className='text-2xl mb-3'>Payment Policy</h2>
             <div dangerouslySetInnerHTML={{ __html: data[0]?.payment_policy }} />
         </div>
