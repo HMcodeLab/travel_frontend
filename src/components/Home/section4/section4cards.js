56805428648969e64185f22ed6f11b753929291e
Davinder9297 <davindergiri765@gmail.com>
2024-07-03 16:50:03 +0530

    updated

--- src/components/Home/section4/section4cards.js
+++ src/components/Home/section4/section4cards.js
@@ -1,20 +1,32 @@
+"use client"
 import { section4Data } from '@/Data/section4data'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './section4.module.css'
 
 const Section4Cards = () => {
+    const [data, setdata] = useState([])
+    useEffect(() => {
+async function fetchdata(){
+    const data=await fetch(process.env.NEXT_PUBLIC_URL+'/apis/packages/category?page=1?limit=10');
+    const response=await data.json();
+    setdata(response?.data?.data)
+    // console.log(response);
+}
+fetchdata()
+    }, [])
+    
     return (
         <div className={styles.all_cards}>
             <span>
                 {
-                    section4Data?.map((val, ind) => {
+                    data?.map((val, ind) => {
                         return (
                             <div className={styles.card} key={ind}>
-                                <Image src={val.img} alt={"..."} height={1000} width={1000} />
+                                <Image src={val.banner_images_main} alt={"..."} height={100} width={100} />
                                 <div className={styles.content}>
-                                    <Image src={val.logo} alt={"..."} height={1000} width={1000} />
-                                    <h3>{val.title}</h3>
+                                    <Image src={val.banner_images} alt={"..."} height={100} width={100} />
+                                    <h3>{val.package_cat_name}</h3>
                                     
                                 </div>
                             </div>
