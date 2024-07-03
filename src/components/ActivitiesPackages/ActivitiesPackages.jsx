2c74131c1c6df2483df46311b548ab41feb6860c
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-07-03 20:01:10 +0530

    updated activities redirection

--- src/components/ActivitiesPackages/ActivitiesPackages.jsx
+++ src/components/ActivitiesPackages/ActivitiesPackages.jsx
@@ -11,7 +11,7 @@ const ActivitiesPackages = ({ data }) => {
   if (data?.length == 0 || data == undefined) {
     return <>Loading...</>;
   }
-    // console.log(data)
+  // console.log(data)
   return (
     <div className="px-[169px] py-6 flex flex-col gap-8 xsm:px-[20px]">
       <div className="flex justify-between">
@@ -32,42 +32,110 @@ const ActivitiesPackages = ({ data }) => {
       <div className="xsm:block hidden">
         <PhoneActivitiesPackage data={data} />
       </div>
-      <Link href={`/destination/${data[0]?.citySlug}/${data[0]?.id}`}>
       <div className="grid grid-cols-3 gap-3 xsm:hidden">
-        <div
-          className="activity-card1 h-[600px] flex font-[Merriweather_Sans] flex-col justify-end text-white rounded-md px-4 py-6 relative overflow-hidden cursor-pointer"
-          style={{
-            backgroundImage: `url(${data[0]?.pdf_image})`,
-          }}
-         
-        >
-         
-          <div className="relative z-10 flex items-center gap-2">
-            <p className="uppercase text-[20px] font-normal tracking-wider">
-              {data[0]?.package_name}
-            </p>
-            <img className="w-8 h-8" src={data[0]?.pdf_image} alt="..." />
-          </div>
-         
-          
-          {/* <p className="relative z-10 text-[12px] capitalize xsm:hidden">
+        <Link href={`/destination/${data[0]?.citySlug}/${data[0]?.id}`}>
+          <div
+            className="activity-card1 h-[600px] flex font-[Merriweather_Sans] flex-col justify-end text-white rounded-md px-4 py-6 relative overflow-hidden cursor-pointer"
+            style={{
+              backgroundImage: `url(${data[0]?.pdf_image})`,
+            }}
+
+          >
+
+            <div className="relative z-10 flex items-center gap-2">
+              <p className="uppercase text-[20px] font-normal tracking-wider">
+                {data[0]?.package_name}
+              </p>
+              <img className="w-8 h-8" src={data[0]?.pdf_image} alt="..." />
+            </div>
+            {/* <p className="relative z-10 text-[12px] capitalize xsm:hidden">
             Let us look at some of the popular adventure sports in Himachal
             Pradesh that take the state by storm
           </p> */}
-        </div>
-        <div className="flex flex-col gap-3">
+          </div>
+        </Link>
+        <Link href={`/destination/${data[1]?.citySlug}/${data[1]?.id}`}>
+          <div className="flex flex-col gap-3">
+            <div
+              className="h-[33%] activity-card3a flex font-[Merriweather_Sans] flex-col justify-end text-white rounded-md px-4 py-6 relative overflow-hidden cursor-pointer"
+              style={{
+                backgroundImage: `url(${data[1]?.pdf_image})`,
+              }}
+            >
+              <div className="relative z-10 flex items-center gap-2">
+                <p className="uppercase text-[20px] font-normal tracking-wider">
+                  {data[1]?.package_name}
+                </p>
+                <img
+                  className="w-8 h-8"
+                  src="/Assets/Icons/activityleaf.svg"
+                  alt="..."
+                />
+              </div>
+              {/* <p className="relative z-10 text-[12px] capitalize xsm:hidden">
+              Let us look at some of the popular adventure sports in Himachal
+              Pradesh that take the state by storm
+            </p> */}
+            </div>
+            <Link href={`/destination/${data[2]?.citySlug}/${data[2]?.id}`}>
+              <div
+                className="h-[33%] activity-card3b flex font-[Merriweather_Sans] flex-col justify-end text-white rounded-md px-4 py-6 relative overflow-hidden cursor-pointer"
+                style={{
+                  backgroundImage: `url(${data[2]?.pdf_image})`,
+                }}
+
+              >
+                <div className="relative z-10 flex items-center gap-2">
+                  <p className="uppercase text-[20px] font-normal tracking-wider">
+                    {data[2]?.package_name}
+                  </p>
+                  <img
+                    className="w-8 h-8"
+                    src="/Assets/Icons/activityleaf.svg"
+                    alt="..."
+                  />
+                </div>
+                {/* <p className="relative z-10 text-[12px] capitalize xsm:hidden">
+              Let us look at some of the popular adventure sports in Himachal
+              Pradesh that take the state by storm
+            </p> */}
+              </div>
+            </Link>
+            <Link href={`/destination/${data[3]?.citySlug}/${data[3]?.id}`}>
+              <div
+                className="h-[33%] activity-card3c flex font-[Merriweather_Sans] flex-col justify-end text-white rounded-md px-4 py-6 relative overflow-hidden cursor-pointer"
+                style={{
+                  backgroundImage: `url(${data[3]?.pdf_image})`,
+                }}
+              >
+                <div className="relative z-10 flex items-center gap-2">
+                  <p className="uppercase text-[20px] font-normal tracking-wider">
+                    {data[3]?.package_name}
+                  </p>
+                  <img
+                    className="w-8 h-8"
+                    src="/Assets/Icons/activityleaf.svg"
+                    alt="..."
+                  />
+                </div>
+                {/* <p className="relative z-10 text-[12px] capitalize xsm:hidden">
+              Let us look at some of the popular adventure sports in Himachal
+              Pradesh that take the state by storm
+            </p> */}
+              </div>
+            </Link>
+          </div>
+        </Link>
+        <Link href={`/destination/${data[4]?.citySlug}/${data[4]?.id}`}>
           <div
-            className="h-[33%] activity-card3a flex font-[Merriweather_Sans] flex-col justify-end text-white rounded-md px-4 py-6 relative overflow-hidden cursor-pointer"
+            className="activity-card2 h-[600px] flex font-[Merriweather_Sans] flex-col justify-end text-white rounded-md px-4 py-6 relative overflow-hidden cursor-pointer"
             style={{
-              backgroundImage: `url(${data[1]?.pdf_image})`,
+              backgroundImage: `url(${data[4]?.pdf_image})`,
             }}
-            onClick={() =>
-              router.push(`/destination/${data[1]?.citySlug}/${data[1]?.id}`)
-            }
           >
             <div className="relative z-10 flex items-center gap-2">
               <p className="uppercase text-[20px] font-normal tracking-wider">
-                {data[1]?.package_name}
+                {data[4]?.package_name}
               </p>
               <img
                 className="w-8 h-8"
@@ -76,85 +144,12 @@ const ActivitiesPackages = ({ data }) => {
               />
             </div>
             {/* <p className="relative z-10 text-[12px] capitalize xsm:hidden">
-              Let us look at some of the popular adventure sports in Himachal
-              Pradesh that take the state by storm
-            </p> */}
-          </div>
-          <div
-            className="h-[33%] activity-card3b flex font-[Merriweather_Sans] flex-col justify-end text-white rounded-md px-4 py-6 relative overflow-hidden cursor-pointer"
-            style={{
-              backgroundImage: `url(${data[2]?.pdf_image})`,
-            }}
-            onClick={() =>
-              router.push(`/destination/${data[2]?.citySlug}/${data[2]?.id}`)
-            }
-          >
-            <div className="relative z-10 flex items-center gap-2">
-              <p className="uppercase text-[20px] font-normal tracking-wider">
-                {data[2]?.package_name}
-              </p>
-              <img
-                className="w-8 h-8"
-                src="/Assets/Icons/activityleaf.svg"
-                alt="..."
-              />
-            </div>
-            {/* <p className="relative z-10 text-[12px] capitalize xsm:hidden">
-              Let us look at some of the popular adventure sports in Himachal
-              Pradesh that take the state by storm
-            </p> */}
-          </div>
-          <div
-            className="h-[33%] activity-card3c flex font-[Merriweather_Sans] flex-col justify-end text-white rounded-md px-4 py-6 relative overflow-hidden cursor-pointer"
-            style={{
-              backgroundImage: `url(${data[3]?.pdf_image})`,
-            }}
-            onClick={() =>
-              router.push(`/destination/${data[3]?.citySlug}/${data[3]?.id}`)
-            }
-          >
-            <div className="relative z-10 flex items-center gap-2">
-              <p className="uppercase text-[20px] font-normal tracking-wider">
-                {data[3]?.package_name}
-              </p>
-              <img
-                className="w-8 h-8"
-                src="/Assets/Icons/activityleaf.svg"
-                alt="..."
-              />
-            </div>
-            {/* <p className="relative z-10 text-[12px] capitalize xsm:hidden">
-              Let us look at some of the popular adventure sports in Himachal
-              Pradesh that take the state by storm
-            </p> */}
-          </div>
-        </div>
-        <div
-          className="activity-card2 h-[600px] flex font-[Merriweather_Sans] flex-col justify-end text-white rounded-md px-4 py-6 relative overflow-hidden cursor-pointer"
-          style={{
-            backgroundImage: `url(${data[4]?.pdf_image})`,
-          }}
-          onClick={() =>
-            router.push(`/destination/${data[4]?.citySlug}/${data[4]?.id}`)
-          }
-        >
-          <div className="relative z-10 flex items-center gap-2">
-            <p className="uppercase text-[20px] font-normal tracking-wider">
-              {data[4]?.package_name}
-            </p>
-            <img
-              className="w-8 h-8"
-              src="/Assets/Icons/activityleaf.svg"
-              alt="..."
-            />
-          </div>
-          {/* <p className="relative z-10 text-[12px] capitalize xsm:hidden">
             Let us look at some of the popular adventure sports in Himachal
             Pradesh that take the state by storm
           </p> */}
-        </div>
+          </div>
+        </Link>
       </div>
-      </Link>
     </div>
   );
 };
