dd63554fb563c1a0bbf12af6b6425c179150514b
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-07-05 14:45:19 +0530

    changes

--- src/components/card/page.jsx
+++ src/components/card/page.jsx
@@ -82,7 +82,7 @@ const Cards = ({val, cityid }) => {
             <Image src={`/Assets/Images/Star.svg`} alt="Star icon" width={16} height={16} />
             <Image src={`/Assets/Images/Star.svg`} alt="Star icon" width={16} height={16} />
             <p className="font-semibold ">{val?.rating} / 5</p>
-            <p className="text-[#969191] text-xs">({val?.reviews || '354'} reviews)</p>
+            <p className="text-[#969191] text-xs">({val?.reviews || 'no'} reviews)</p>
           </div>
         </div>
         <div className="flex justify-between items-center h-9">
